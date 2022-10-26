c84f3f27e65aeeed50e505b148e8685e5a616793
we661512 <we661512@login18-1.hpc.itc.rwth-aachen.de>
2022-10-26 21:33:22 +0200

    Move and adapt server slurm files

--- src/create_execution_file.py
+++ src/create_execution_file.py
@@ -5,13 +5,13 @@ try:
 except FileNotFoundError:
     pass
 
-iterations = 4
+iterations = 25
 algorithms = ['GOT', 'L2', 'L2-inv']
 
 text = ""
 
 for algo in algorithms:
     for seed in range(iterations):
-        text += f'python3 -u main.py {algo} {seed}\n'
+        text += f'python3 -u main.py {algo} {seed} --iterations 1000 --regularize\n'
 with open('experiment_commands.txt', 'a') as file:
     file.write(text)
