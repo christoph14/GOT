4cd9f3b6af0982b30ddf233f1c0ed2d8f17a1ab1
christoph14 <christoph.sandrock12@gmail.com>
2023-03-15 11:07:57 +0100

    Update check_permutation_matrix (use np.allclose)

--- src/utils/_validation.py
+++ src/utils/_validation.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 
-def check_permutation_matrix(P):
+def check_permutation_matrix(P, atol=1e-3):
     P = np.array(P)
     if not P.ndim == 2:
         raise ValueError("P.ndim is not 2.")
     if not P.shape[0] == P.shape[1]:
         raise ValueError("P is not symmetric.")
-    if not (P.sum(axis=0) == 1).all():
+    if not np.allclose(P.sum(axis=0), 1, atol=atol):
         raise ValueError("Not all columns sum up to 1.")
-    if not (P.sum(axis=1) == 1).all():
+    if not np.allclose(P.sum(axis=1), 1, atol=atol):
         raise ValueError("Not all rows sum up to 1.")
-    if not ((P == 1) | (P == 0)).all():
+    if not (np.isclose(P, 0, atol=atol) | np.isclose(P, 1, atol=atol)).all():
         raise ValueError("All entries must be either 0 or 1.")
-    return P.astype(int)
+    return np.round(P).astype(int)
 
 
 def check_soft_assignment(P, atol=0.001):
