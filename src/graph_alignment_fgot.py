05b7bc9b9b1d2e7b563bdbd9b39fb5e93c79a879
christoph14 <christoph.sandrock12@gmail.com>
2023-03-18 13:17:18 +0100

    Small changes

--- src/graph_alignment_fgot.py
+++ src/graph_alignment_fgot.py
@@ -16,26 +16,23 @@ parser = argparse.ArgumentParser(description='Evaluates graph alignment algorith
 parser.add_argument('strategies', type=str, nargs='+', help='the strategies to be performed')
 parser.add_argument('seed', type=int, help='the used random seed')
 # Experiment parameters
-parser.add_argument('--within_probability', dest='within_probability', type=float, default=0.7)
-parser.add_argument('--between_probability', dest='between_probability', type=float, default=0.1)
-parser.add_argument('--graph_size', dest='graph_size', type=int, default=100)
+parser.add_argument('--within_probability', type=float, default=0.7)
+parser.add_argument('--between_probability', type=float, default=0.1)
+parser.add_argument('--graph_size', type=int, default=100)
 # GOT parameters
-parser.add_argument('--alpha', dest='alpha', type=float, default=0.1, help='the regularization factor')
-parser.add_argument('--it', dest='it', type=int, default=10, help='number of Sinkhorn iterations')
-parser.add_argument('--tau', dest='tau', type=float, default=5, help='the Sinkhorn parameter')
-parser.add_argument('--sampling_size', dest='sampling_size', type=int, default=30, help='the sampling size')
-parser.add_argument('--iterations', dest='iterations', type=int, default=3000, help='the number of iterations')
-parser.add_argument('--lr', dest='lr', type=float, default=0.2, help='the learning rate')
+parser.add_argument('--alpha', type=float, default=0.1, help='the regularization factor')
+parser.add_argument('--it', type=int, default=10, help='number of Sinkhorn iterations')
+parser.add_argument('--tau', type=float, default=5, help='the Sinkhorn parameter')
+parser.add_argument('--sampling_size', type=int, default=30, help='the sampling size')
+parser.add_argument('--iterations', type=int, default=3000, help='the number of iterations')
+parser.add_argument('--lr', type=float, default=0.2, help='the learning rate')
 # General parameters
-parser.add_argument('--path', dest='path', type=str, default='../results/', help='the path to store the output files')
-parser.add_argument('--ignore_log', dest='ignore_log', action='store_const', const=True, default=False, help='disables the log')
-parser.add_argument('--allow_soft_assignment', dest='allow_soft_assignment', action='store_const', const=True,
+parser.add_argument('--path', type=str, default='../results/', help='the path to store the output files')
+parser.add_argument('--ignore_log', action='store_const', const=True, default=False, help='disables the log')
+parser.add_argument('--allow_soft_assignment', action='store_const', const=True,
                     default=False, help='allow soft assignment instead of a permutation matrix')
 parser.add_argument('--reset_results', action='store_const', const=True, default=False, help='delete old results')
 args = parser.parse_args()
-
-# Create results folder
-os.makedirs(args.path, exist_ok=True)
 
 # Get strategies
 strategy_names = args.strategies
@@ -48,11 +45,6 @@ if not args.ignore_log:
     print('Path:', args.path)
 
 # Create dictionaries
-w2_errors = {}
-l2_errors = {}
-for name in strategy_names:
-    w2_errors[name] = []
-    l2_errors[name] = []
 data = []
 
 # Create random generator
@@ -77,8 +69,6 @@ for p in p_values:
         # Calculate and save different loss functions
         w2_error = w2_loss(L1, L2, P_estimated)
         l2_error = l2_loss(L1, L2, P_estimated)
-        w2_errors[name].append(w2_error)
-        l2_errors[name].append(l2_error)
         data.append(
             {'strategy' : name,
              'seed' : args.seed,
@@ -91,6 +81,7 @@ for p in p_values:
         print(f'p = {p:.2f} done.')
 
 # Save results in database
+os.makedirs(args.path, exist_ok=True)
 con = sqlite3.connect(f'{args.path}/fgot_results.db', timeout=100)
 cur = con.cursor()
 if args.reset_results:
