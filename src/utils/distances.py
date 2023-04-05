7cf80ca17c790249b7b3b9ede7bff509841fbe8f
christoph14 <christoph.sandrock12@gmail.com>
2023-04-05 13:35:54 +0200

    Remove GW distance

--- src/utils/distances.py
+++ src/utils/distances.py
@@ -1,4 +1,3 @@
-import networkx as nx
 import numpy as np
 import scipy.linalg as slg
 
@@ -21,30 +20,4 @@ def wasserstein_distance(A, B):
     root = slg.sqrtm(A)
     result = np.trace(A) + np.trace(B) - 2 * np.trace(slg.sqrtm(root @ B @ root))
     distance = np.abs(result.real)
-    return distance
-
-
-def gw_distance(G1, G2):
-    """Compute the Gromov-Wasserstein distance between two graphs.
-    Use the shortest paths as structure similarity matrices.
-
-    Parameters
-    ----------
-    G1 : nx.graph
-        First graph.
-    G2 : nx.graph
-        Second graph.
-
-    Returns
-    -------
-    distance : float
-        The computed Gromov-Wasserstein distance between G1 and G2.
-    """
-
-    # Compute the shortest path matrices
-    C1 = nx.floyd_warshall_numpy(G1)
-    C2 = nx.floyd_warshall_numpy(G2)
-
-    n = len(C1)
-    distance = (np.linalg.norm(C1 - C2, ord='fro') / n)**2
     return distance
