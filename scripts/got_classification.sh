b27647619652e375617b326fbedbec55d121b82f
christoph14 <christoph.sandrock12@gmail.com>
2023-05-15 11:42:56 +0200

    New script

---
+++ scripts/got_classification.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=got
+#SBATCH --output=../log_slurm/got_classification_%A_%a.log
+#SBATCH --array=0-24
+#SBATCH --time=06:00:00
+#SBATCH --partition=c18m
+#SBATCH --account=thes1398
+#SBATCH --ntasks=6
+#SBATCH --cpus-per-task=1
+
+export CONDA_ROOT=$HOME/miniconda3
+. $CONDA_ROOT/etc/profile.d/conda.sh
+export PATH="$CONDA_ROOT/bin:$PATH"
+conda activate graph
+
+cd ~/GOT/src || exit
+
+algorithms=(GOT L2 GW fGOT QAP random)
+for algo in ${algorithms[*]}
+do
+  srun -N1 -n1 -c1 --exact python -u graph_classification.py "$algo" --seed "$SLURM_ARRAY_TASK_ID" --path ../results --allow_soft_assignment &
+done
