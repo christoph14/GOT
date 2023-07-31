0fb8f8cf04925fdb5f1fe9d4b21e994efc381e8e
christoph14 <christoph.sandrock12@gmail.com>
2023-07-31 17:09:15 +0200

    Use slurm array

--- scripts/compute_distances.sh
+++ scripts/compute_distances.sh
@@ -2,10 +2,11 @@
 
 #SBATCH --job-name=fgot
 #SBATCH --output=../log_slurm/fgot_distances_%A.log
+#SBATCH --array=0-9
 #SBATCH --time=12:00:00
 #SBATCH --partition=c18m
 #SBATCH --account=thes1398
-#SBATCH --ntasks=10
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 
 export CONDA_ROOT=$HOME/miniconda3
@@ -15,7 +16,6 @@ conda activate graph
 
 cd ../src || exit
 
-for epsilon in 0.002 0.004 0.006 0.008 0.01 0.02 0.04 0.06 0.08 0.1; do
-    srun --exclusive --ntasks=1 --nodes=1 --cpus-per-task="$SLURM_CPUS_PER_TASK" python -u create_distance_matrix.py fGOT "$1" --filter "$2" --epsilon $epsilon &
-done
-wait
+epsilon_range=(0.002 0.004 0.006 0.008 0.01 0.02 0.04 0.06 0.08 0.1)
+
+srun --exclusive --ntasks=1 --nodes=1 --cpus-per-task="$SLURM_CPUS_PER_TASK" python -u create_distance_matrix.py fGOT "$1" --filter "$2" --epsilon ${epsilon_range[$SLURM_ARRAY_TASK_ID]}
