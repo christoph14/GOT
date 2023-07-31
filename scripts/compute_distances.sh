5de26ad8edb06300915d7bd4d0b43ae2c8120480
christoph14 <christoph.sandrock12@gmail.com>
2023-07-31 16:44:49 +0200

    Update epsilon range

--- scripts/compute_distances.sh
+++ scripts/compute_distances.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=06:00:00
 #SBATCH --partition=c18m
 #SBATCH --account=thes1398
-#SBATCH --ntasks=5
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=24
 
 export CONDA_ROOT=$HOME/miniconda3
@@ -15,7 +15,7 @@ conda activate graph
 
 cd ../src || exit
 
-for epsilon in 0.003 0.006 0.009 0.015 0.03; do
+for epsilon in 0.002 0.004 0.006 0.008 0.01 0.02 0.04 0.06 0.08 0.1; do
     srun --exclusive --ntasks=1 --nodes=1 --cpus-per-task="$SLURM_CPUS_PER_TASK" python -u create_distance_matrix.py fGOT "$1" --filter "$2" --epsilon $epsilon &
 done
 wait
